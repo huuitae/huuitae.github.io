fb58c5386a2e1a42d051463abc7e65eeab4db5d5
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-23 00:15:12 +0900

    feat: giscus 연동으로 댓글 기능 추가

---
+++ src/components/comment/GiscusComment.tsx
@@ -0,0 +1,48 @@
+import Giscus from '@giscus/react'
+import { useEffect, useState } from 'react'
+
+export default function GiscusComment() {
+  const [theme, setTheme] = useState<string | null>()
+
+  useEffect(() => {
+    const currentTheme = document.documentElement.getAttribute('data-theme')
+    setTheme(currentTheme)
+
+    const themeObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'data-theme') {
+          setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+        }
+      })
+    })
+
+    themeObserver.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['data-theme'],
+    })
+
+    return () => {
+      themeObserver.disconnect()
+    }
+  }, [])
+
+  return (
+    <div>
+      <Giscus
+        repo="huuitae/huuitae.github.io"
+        repoId="R_kgDOQGUdEA"
+        category="Announcements"
+        categoryId="DIC_kwDOQGUdEM4Cw7zj"
+        mapping="pathname"
+        term="Welcome to @giscus/react component!"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme={theme || 'light'}
+        lang="ko"
+        loading="lazy"
+      />
+    </div>
+  )
+}
